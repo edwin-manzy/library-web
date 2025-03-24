761281d47082066cbdc9fee9b928ebaab85e9ad1
Edwin Chiwona <edwindijas@hotmail.com>
2025-03-24 20:13:48 +0200

    improve menu

--- src/common/components/molecules/menu/menu.tsx
+++ src/common/components/molecules/menu/menu.tsx
@@ -1,18 +1,22 @@
 import { ReactElement } from 'react';
 
+import { Link, NavLink } from 'react-router';
+
 import { MenuProps } from './menu.types';
 
-export const Menu = ({ items }: MenuProps): ReactElement | null => {
+export const Menu = ({ items, isNav = false, baseUrl }: MenuProps): ReactElement | null => {
 
   if (items === undefined) {
     return null;
   }
 
   if (!Array.isArray(items)) {
-    const { label, children } = items;
+    const { label, children, link } = items;
+    const Comp = isNav ? NavLink : Link;
+    const lnk = baseUrl ? baseUrl + '/' + link : link;
     return <li>
-      { label }
-      <Menu items={children} />
+      <Comp to={lnk}  >{ label }</Comp>
+      <Menu baseUrl={baseUrl ? lnk : undefined} isNav={isNav} items={children} />
     </li>;
   }
 
@@ -20,9 +24,9 @@ export const Menu = ({ items }: MenuProps): ReactElement | null => {
     {
       items.map((menu, index) => {
         if (Array.isArray(menu)) {
-          return <li key={index} ><Menu items={menu} /></li>;
+          return <li key={index} ><Menu baseUrl={baseUrl}  isNav={isNav} items={menu} /></li>;
         }
-        return <Menu items={menu} key={menu.label} />;
+        return <Menu baseUrl={baseUrl} isNav={isNav} items={menu} key={menu.label} />;
       })
     }
   </ul>;
--- src/common/components/molecules/menu/menu.types.ts
+++ src/common/components/molecules/menu/menu.types.ts
@@ -2,6 +2,7 @@ import { MenuItem } from 'src/common/interfaces/components';
 
 export interface MenuProps {
   items: MenuItem | undefined | Array<MenuItem | MenuItem[]>
-  baseUrl?: string,
-  isChild?: boolean
+  baseUrl?: string;
+  isChild?: boolean;
+  isNav: boolean
 }
